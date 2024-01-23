0334a31c7361776316c060ce7b8bf3af3edfa3cf
Web Dev <125798807+WebProjDeveloper@users.noreply.github.com>
2024-01-23 22:41:31 +0100

    Update V1

--- Activity7/customer-front-thymeleaf-app/src/main/java/ma/emsi/customerfrontthymeleafapp/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package ma.emsi.customerfrontthymeleafapp.repository;
-
-import ma.emsi.customerfrontthymeleafapp.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
