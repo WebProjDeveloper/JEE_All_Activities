ff10db1bb598ce3f265631cdde4c6295a42ddb94
Web Dev <125798807+WebProjDeveloper@users.noreply.github.com>
2024-01-23 23:10:59 +0100

    Update V2

---
+++ Activity7/emsi-sdia-app/customer-front-thymeleaf-app/src/main/java/ma/emsi/customerfrontthymeleafapp/CustomerFrontThymeleafAppApplication.java
@@ -0,0 +1,26 @@
+package ma.emsi.customerfrontthymeleafapp;
+
+import ma.emsi.customerfrontthymeleafapp.entities.Customer;
+import ma.emsi.customerfrontthymeleafapp.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CustomerFrontThymeleafAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerFrontThymeleafAppApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(CustomerRepository customerRepository){
+        return args -> {
+            customerRepository.save(Customer.builder().name("Housna").email("Housna@gmail.com").build());
+            customerRepository.save(Customer.builder().name("Ouassima").email("Ouassima@gmail.com").build());
+            customerRepository.save(Customer.builder().name("Meriem").email("Meriem@gmail.com").build());
+            customerRepository.save(Customer.builder().name("Hanae").email("Hanae@gmail.com").build());
+        };
+    }
+}
